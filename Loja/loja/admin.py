2a4b983cc8452e3a43f71c72826e0acab7f893a8
Arthur Medeiros Pinheiro <20231011110020@ifrn.local>
2025-05-06 14:25:51 -0300

    Adição dos filtros de data, classe que filtra os produtos por data, mostra informações de registro na listagem e muda a presentação de campos vazios, adição de limite de 100 carac. para a msgpromocao, adição de uma função que exclui o fabricante e a imagem no cadastro de produto, uma função que pesquisa produtos na aba de produtos e uma função que exclui a msgpromocao

--- Loja/loja/admin.py
+++ Loja/loja/admin.py
@@ -3,6 +3,17 @@ from django.contrib import admin
 from django.contrib import admin #isso já vai estar existindo noarquivo
 #Register your models here.
 from .models import * #imporata nossos models
-admin.site.register(Fabricante) #adiciona a interface do adm
+class FabricanteAdmin(admin.ModelAdmin):
+    # Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao',
+    'preco', 'categoria',)
+    empty_value_display = 'Vazio'
+    fields = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',) #exclui o fabricante e imagem
+    search_fields = ('Produto',) #esse pesquisa os produtos na aba de produtos
+    # exclude =('msgPromocao',) #esse exclui a mensagem de promoção
+admin.site.register(Fabricante, FabricanteAdmin) #adiciona a interface do adm
 admin.site.register(Categoria)
-admin.site.register(Produto)
+admin.site.register(Produto, ProdutoAdmin)
