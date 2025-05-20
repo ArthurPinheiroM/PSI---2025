c18a03845571037439d03e98d772e043e6bfddac
Arthur Medeiros Pinheiro <20231011110020@ifrn.local>
2025-05-20 15:34:52 -0300

    Adição de mecanismo de busca

--- Loja/loja/views/ProdutoView.py
+++ Loja/loja/views/ProdutoView.py
@@ -1,13 +1,22 @@
 from django.http import HttpResponse
 from loja.models import Produto
+from datetime import timedelta, datetime
+from django.utils import timezone
 def list_produto_view(request, id=None):
+    dias = request.GET.get("dias")
     produto = request.GET.get("produto")
     destaque = request.GET.get("destaque")
     promocao = request.GET.get("promocao")
     categoria = request.GET.get("categoria")
     fabricante = request.GET.get("fabricante")
-    produtos = Produto.objects.filter(Produto=produto)
-    print(produtos)
+    produtos = Produto.objects.all()
+    if produto is not None:
+        produtos = produtos.filter(Produto__contains=produto)
+    if promocao is not None:
+        produtos = produtos.filter(promocao=promocao)
+    if destaque is not None:
+        produtos = produtos.filter(destaque=destaque)  
+    # print(produtos)
     if destaque is not None:
         print(destaque)
     if produto is not None:
@@ -15,11 +24,18 @@ def list_produto_view(request, id=None):
     if promocao is not None:
         print(promocao)
     if categoria is not None:
-        print(categoria)
+        produtos = produtos.filter(categoria__Categoria=categoria)
     if fabricante is not None:
-        print(fabricante)
+        produtos = produtos.filter(fabricante__Fabricante=fabricante)
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    
+    if dias is not None:
+        now = timezone.now()
+        print(dias)
+        now = now - timedelta(days = int(dias))
+        produtos = produtos.filter(criado_em__gte=now)
+    print(produtos)
     if id is None:
-        id = 0
-    # if id is None:
-    #     return HttpResponse('<h1>Nenhum id foi informado</h1>') opcional
-    return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+        return HttpResponse('<h1>Nenhum id foi informado</h1>') #opcional
+    # return HttpResponse('<h1>Produto de id %s!</h1>' % id)
