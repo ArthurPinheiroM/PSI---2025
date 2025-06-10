3ee69766e9da939a048a110be3ce2e8fa107a1c2
Arthur Medeiros Pinheiro <20231011110020@ifrn.local>
2025-06-10 15:49:42 -0300

    Adição de mais HTML's (os templates)

--- Loja/loja/views/ProdutoView.py
+++ Loja/loja/views/ProdutoView.py
@@ -1,7 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
+def edit_produto_view(request, id=None):
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = { 'produto': produto }
+    return render(request, template_name='produto/produto-edit.html', context=context, status=200)
+
 def list_produto_view(request, id=None):
     dias = request.GET.get("dias")
     produto = request.GET.get("produto")
@@ -29,13 +38,17 @@ def list_produto_view(request, id=None):
         produtos = produtos.filter(fabricante__Fabricante=fabricante)
     if id is not None:
         produtos = produtos.filter(id=id)
-    
     if dias is not None:
         now = timezone.now()
         print(dias)
         now = now - timedelta(days = int(dias))
         produtos = produtos.filter(criado_em__gte=now)
-    print(produtos)
-    if id is None:
-        return HttpResponse('<h1>Nenhum id foi informado</h1>') #opcional
+    # print(produtos)
+    
+    context = {'produtos': produtos}
+    return render(request, template_name='produto/produto.html', context=context, status=200)
+    
+
+    # if id is None:
+    #     return HttpResponse('<h1>Nenhum id foi informado</h1>') #opcional!
     # return HttpResponse('<h1>Produto de id %s!</h1>' % id)
